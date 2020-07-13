5fe068425cbafd3fdbe815c5e54b5914cbf99a17
Learner <vivek.gurukul1982@gmail.com>
2020-07-13 15:02:27 -0700

    Update find-all-the-lonely-nodes.java

--- find-all-the-lonely-nodes.java
+++ find-all-the-lonely-nodes.java
@@ -22,24 +22,6 @@ class find-all-the-lonely-nodes {
     public List<Integer> getLonelyNodes(TreeNode root) {
         List<Integer> res = new LinkedList<>();
         if(root==null) return res;
-        Queue<TreeNode> q = new LinkedList<>();
-        q.offer(root);
-        while(!q.isEmpty()){
-            TreeNode node = q.poll();
-            if(node.left==null && node.right!=null) 
-                res.add(node.right.val);
-            if(node.left!=null && node.right==null)
-                res.add(node.left.val);
-            if(node.left!=null) q.offer(node.left);
-            if(node.right!=null) q.offer(node.right);
-        }
-        return res;
-    }
-    
-     // tc -> n, sc-> n
-    public List<Integer> getLonelyNodes(TreeNode root) {
-        List<Integer> res = new LinkedList<>();
-        if(root==null) return res;
         lonelyNodes(root, res, false);
         return res;
     }
@@ -51,4 +33,22 @@ class find-all-the-lonely-nodes {
         lonelyNodes(root.left, res, root.left!=null && root.right==null);
         lonelyNodes(root.right, res, root.right!=null && root.left==null);
     }
+    
+    // tc -> n, sc-> n
+   public List<Integer> getLonelyNodes(TreeNode root) {
+       List<Integer> res  = new LinkedList<>();
+       setLonelyNodes(root, res, false); // root is not lonely
+       return res;
+   }
+   
+   private void setLonelyNodes(TreeNode root, List<Integer> res,
+                             boolean isLonely){
+       if(root==null) return;
+       
+       if(isLonely) res.add(root.val);
+           
+       setLonelyNodes(root.left, res, root.right==null);
+       setLonelyNodes(root.right, res, root.left==null);
+   }
+    
 }
