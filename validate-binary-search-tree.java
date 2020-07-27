51f0e204d69200086213edae5b920890f2b2961d
Learner <vivek.gurukul1982@gmail.com>
2020-07-27 16:39:14 -0700

    Update validate-binary-search-tree.java

--- validate-binary-search-tree.java
+++ validate-binary-search-tree.java
@@ -28,4 +28,17 @@ class validate-binary-search-tree {
         }
         return true;
     }
+    
+    // tc -> n, sc-> n
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    
+    private boolean isValidBST(TreeNode root, long min, long max){
+        if(root==null) return true;
+        if(root.val <= min || root.val >= max) return false;
+        boolean isLeft = isValidBST(root.left, min, root.val);
+        boolean isRight = isValidBST(root.right, root.val, max);
+        return isLeft && isRight;
+    }
 }
