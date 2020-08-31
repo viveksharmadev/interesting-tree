91a89485e404cb456db32b7c7882bdd853709535
Learner <vivek.gurukul1982@gmail.com>
2020-08-31 12:21:38 -0700

    Create delete-node-in-a-bst.java

---
+++ delete-node-in-a-bst.java
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/delete-node-in-a-bst/
+class delete-node-in-a-bst {
+    // tc -> logn, sc-> logn
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root==null) return null;
+        
+        if(key < root.val) {
+            root.left = deleteNode(root.left, key);
+        }else if(key > root.val){
+            root.right = deleteNode(root.right, key);
+        }else{
+            if(root.left==null){
+                return root.right;
+            }else if(root.right==null){
+                return root.left;
+            }else{
+                TreeNode nextSmallest = root.right;
+                while(nextSmallest.left!=null) nextSmallest = nextSmallest.left;
+                nextSmallest.left = root.left;
+                return root.right;
+            }
+        }
+        
+        return root;
+    }
+}
