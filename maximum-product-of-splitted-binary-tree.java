046f2443fc3a919621a59a4e2b38909d65c971b9
Learner <vivek.gurukul1982@gmail.com>
2020-09-03 10:32:09 -0700

    Update maximum-product-of-splitted-binary-tree.java

--- maximum-product-of-splitted-binary-tree.java
+++ maximum-product-of-splitted-binary-tree.java
@@ -17,3 +17,42 @@ class maximum-product-of-splitted-binary-tree {
         return sub;
     }
 }
+
+// Cleaner
+
+class Solution {
+    // tc -> n, sc-> n
+    public int maxProduct(TreeNode root) {
+        long[] result = new long[1];
+        
+        long total = getTotal(root);
+        
+        maxProductInTree(root, result, total);
+        
+        return (int)(result[0] % ((int)1e9+7));
+    }
+    
+    private long getTotal(TreeNode root){
+        if(root==null) return 0;
+        
+        long left = getTotal(root.left);
+        long right = getTotal(root.right);
+        
+        return left + right + root.val;
+    }
+    
+    private long maxProductInTree(TreeNode root, long[] result,
+                                 long total){
+        if(root==null) return 0;
+        
+        long left = maxProductInTree(root.left, result, total);
+        long right = maxProductInTree(root.right, result, total);
+        
+        long subSum = left + right + root.val;
+        
+        long product = subSum * (total-subSum);
+        result[0] = Math.max(result[0], product);
+        
+        return subSum;
+    }
+}
