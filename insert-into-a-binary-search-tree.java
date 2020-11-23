c4219c622bd21843288596d21454f1bd5da69813
Learner <vivek.gurukul1982@gmail.com>
2020-11-22 16:40:00 -0800

    Update insert-into-a-binary-search-tree.java

--- insert-into-a-binary-search-tree.java
+++ insert-into-a-binary-search-tree.java
@@ -73,3 +73,23 @@ class Solution {
         return root;
     }
 }
+
+// Recursive, may be easier to understand
+class Solution {
+    // tc -> logn, sc-> logn
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if(root==null) return new TreeNode(val);
+        
+        if(val < root.val) root.left = insertIntoBST(root.left, val);
+        
+        else if(val > root.val) root.right = insertIntoBST(root.right, val);
+        
+        else{
+            if(root.left==null) root.left = new TreeNode(val);
+            
+            else if(root.right==null) root.right = new TreeNode(val);
+        }
+        
+        return root;
+    }
+}
